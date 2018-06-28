848725f7575caedd02e3fc75c8e448abb1931b24
Brenden Thornton <thorntonbrenden@gmail.com>
2018-06-28 03:40:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,7 +122,6 @@ def over?(board)
   if won?(board) || full?(board) || draw?(board) 
     return true 
   end 
-  false 
 end 
 
 #This checks to see if X or O has won the game and tells us who is the winner
