8c4c55b3b72289bccedfc89482b84189057cb1b3
Brenden Thornton <thorntonbrenden@gmail.com>
2018-06-28 03:37:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -142,7 +142,7 @@ end
 def play(board)
   if(over?(board)) 
     if winner(board)
-      puts "Congratulations #{winner(board)}"
+      puts "Congratulations #{winner(board)}!"
     elsif draw?(board)
       puts "Cat's Game!"
     end 
